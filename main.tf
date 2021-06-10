fcb0b6788072896f3988b70c4fdf0429ac80a9bc
Xavier Sunyer Balsebre <xsunyer@ctaima.com>
2021-06-10 15:07:28 +0200

    added manifest to deploy api

--- main.tf
+++ main.tf
@@ -3,5 +3,14 @@ provider "azurerm" {
 }
 
 terraform {
-    backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }    
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "azurerm" {}
 }
